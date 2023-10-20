bd9034f2078819308d49d3ab079ff7e1a05d1079
Wai Linn Kyaw <stephensuu03@gmail.com>
2023-10-20 15:56:27 +0630

    a

--- assets/js/script.js
+++ assets/js/script.js
@@ -85,3 +85,47 @@ $('.responsive1').slick({
         }
     ]
 });
+
+$('.responsive2').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    prevArrow: '<i class="fas fa-angle-left left_arrow2">',
+    nextArrow: '<i class="fas fa-angle-right right_arrow2">',
+    // rtl: true,
+    responsive: [
+        {
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false,
+        }
+        },
+        {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+            },
+        {
+        breakpoint: 600,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+        },
+        {
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+        }
+    ]
+});
