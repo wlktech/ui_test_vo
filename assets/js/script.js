b157da0d3f88f932b677119820ebf6fb7e08b5fe
Wai Linn Kyaw <stephensuu03@gmail.com>
2023-10-20 09:42:53 +0630

    a

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,23 +12,30 @@ $('.responsive').slick({
         {
         breakpoint: 1024,
         settings: {
-            slidesToShow: 3.5,
+            slidesToShow: 2,
             slidesToScroll: 1,
             infinite: true,
             dots: false,
         }
         },
         {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+            },
+        {
         breakpoint: 600,
         settings: {
-            slidesToShow: 2.5,
+            slidesToShow: 1,
             slidesToScroll: 1
         }
         },
         {
         breakpoint: 480,
         settings: {
-            slidesToShow: 2.5,
+            slidesToShow: 1,
             slidesToScroll: 1
         }
         }
