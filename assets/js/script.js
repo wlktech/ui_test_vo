a80d6d23aab0a8429512cef99248e7b200556341
Wai Linn Kyaw <stephensuu03@gmail.com>
2023-10-20 14:37:01 +0630

    a

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,50 @@ $('.responsive').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
+    prevArrow: '<i class="fas fa-angle-left left_arrow">',
+    nextArrow: '<i class="fas fa-angle-right right_arrow">',
+    // rtl: true,
+    responsive: [
+        {
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false,
+        }
+        },
+        {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+            },
+        {
+        breakpoint: 600,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+        },
+        {
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+        }
+    ]
+});
+
+$('.responsive1').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
     prevArrow: '<i class="fas fa-angle-left left_arrow1">',
     nextArrow: '<i class="fas fa-angle-right right_arrow1">',
     // rtl: true,
@@ -12,7 +56,7 @@ $('.responsive').slick({
         {
         breakpoint: 1024,
         settings: {
-            slidesToShow: 2,
+            slidesToShow: 1,
             slidesToScroll: 1,
             infinite: true,
             dots: false,
